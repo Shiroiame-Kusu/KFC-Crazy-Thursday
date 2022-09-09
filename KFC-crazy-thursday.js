fccf0981965992c0c512dcc729a0a565e9b313c2
楠ちゃん <amenai@bwmc.live>
2022-09-09 18:08:05 +0800

    Ver 1.0.2: Im Boring

--- KFC-crazy-thursday.js
+++ KFC-crazy-thursday.js
@@ -7,7 +7,6 @@ var console_ticket = `
  ticket      @ `
 var KFC = 'KFC-crazy-thursday.js:50'
 
-
 var styleConsoleError = ``
 var styleConsoleError1 = `
 color: rgb(255,255,255);
@@ -19,9 +18,10 @@ text-decoration:underline;
 `
 var footer = document.getElementById("footer")
 
-
 var tempDate = new Date();
 var day = tempDate.getDay();
+
+
 
 
 
